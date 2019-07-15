9070a620e460f01ec938a7cd703da041abc140c5
KrupoderovMikhail <mishka.crash@gmail.com>
2019-07-15 17:11:33 +0500

    Added comments to improve readability and code understanding.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,14 @@
 
+/* Хранение url ресурсов */
 var messageApi = Vue.resource('/message{/id}');
 
+/* Отображение одного объекта */
 Vue.component('message-row', {
     props: ['message'], // properties
     template: '<div> <i>({{ message.id }})</i> {{ message.text }}</div>'
 });
 
+/* Отображение данных с помощью цикла */
 Vue.component('messages-list', {
     props: ['messages'],
     template:
@@ -21,6 +24,7 @@ Vue.component('messages-list', {
     }
 });
 
+/* Здесь происходит передача данных в index.html */
 var app = new Vue({
   el: '#app', // id html тега
   template: '<messages-list :messages="messages" />',
